e857b7a7807a4318515ea8959554cdbaf7c2d466
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2022-04-06 14:32:27 +0200

    config file

--- tis/config.py
+++ tis/config.py
@@ -1,5 +1,21 @@
 import configparser
+import os
 
+
+# DEFAULT PARAMETERS
+parameters = {"data_folder": "/",
+              }
+
+
+
+# READ PARAMETERS FROM .CONF FILE
 parser = configparser.ConfigParser()
 
-print(parser.read("../tis.conf"))
+config_file_path = os.path.join("","tis.conf")
+
+parser.read(config_file_path)
+
+for section_name in parser.sections():
+    if section_name == "USER PARAMETERS":
+        for key, value in parser.items(section_name):
+            print(key,value)
